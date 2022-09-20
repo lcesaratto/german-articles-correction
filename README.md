efe60be98222fe34b95d6b7bda760e32864b6ad8
Lisandro Cesaratto <lisandrocesaratto95@gmail.com>
2022-09-20 23:12:30 +0200

    show articles info and removed references

--- README.md
+++ README.md
@@ -1,24 +1,4 @@
-References:
+German grammar corrector for definite and indefinite articles.
 
-1. Deepset.ai German BERT model: https://www.deepset.ai/german-bert
-2. Corresponding publication: https://aclanthology.org/2020.coling-main.598/
-3. Hugging Face pretrained german BERT model: https://huggingface.co/bert-base-german-cased
-4. Grammar and spelling checker: https://textgears.com/grammatik-uberprufen-online
-5. German Wikipedia dump: https://dumps.wikimedia.org/dewiki/20220520/
-6. Preprocessed wikipedia corpus: https://github.com/GermanT5/wikipedia2corpus
-7. https://v2.spacy.io/api/annotation
-8. https://spacy.io/api/morphology#morphanalysis
-9. https://spacy.io/usage/linguistic-features#pos-tagging
-10. https://huggingface.co/docs/transformers/model_doc/bert#transformers.BertForMaskedLM
-11. https://spacy.io/models/de#de_core_news_lg
-12. https://explosion.ai/blog/ud-benchmarks-v3-2
-13. https://universaldependencies.org/treebanks/de_hdt/index.html
-14. https://stackoverflow.com/questions/64156202/add-dense-layer-on-top-of-huggingface-bert-model
-15. https://huggingface.co/docs/transformers/v4.21.3/en/model_doc/bert#transformers.BertModel
-16. https://sunilchomal.github.io/GECwBERT/
-
+How to install pytorch locally:
 pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
-
-python -m spacy download de_dep_news_trf
-python -m spacy download de_core_news_md
-python -m spacy download de_core_news_lg
--- src/german_grammar_correction/01_preprocess_data.py
+++ src/german_grammar_correction/01_preprocess_data.py
@@ -195,25 +195,45 @@ class DataGenerator:
                 "data/raw_data_short_multiple_masks_per_sentence.csv", index=False)
 
     @staticmethod
+    def show_info(run=False):
+        if run:
+            df = pd.read_csv(
+                "data/raw_data_short_one_mask_per_sentence.csv", nrows=None)
+
+            df = df[(df["pos_tag"] == "ART") & (
+                df["morph_pron_type"] == "Art")]
+
+            lowercase_tokens = ["die", "der", "das", "den", "dem", "des"]
+
+            print(df[df["masked_token"].isin(lowercase_tokens)][["masked_token",
+                                                                 "morph_definite",
+                                                                 "morph_case",
+                                                                 "morph_gender",
+                                                                 "morph_number"]].drop_duplicates().sort_values(["morph_case",
+                                                                                                                 "morph_definite",
+                                                                                                                 "morph_case",
+                                                                                                                 "morph_gender",
+                                                                                                                 "morph_number"]).reset_index(drop=True))
+
+            lowercase_tokens = ["eine", "einen",
+                                "ein", "einer", "einem", "eines"]
+
+            print(df[df["masked_token"].isin(lowercase_tokens)][["masked_token",
+                                                                 "morph_definite",
+                                                                 "morph_case",
+                                                                 "morph_gender",
+                                                                 "morph_number"]].drop_duplicates().sort_values(["morph_case",
+                                                                                                                 "morph_definite",
+                                                                                                                 "morph_case",
+                                                                                                                 "morph_gender",
+                                                                                                                 "morph_number"]).reset_index(drop=True))
+
+    @staticmethod
     def add_wrong_sentences_to_training_data_one_mask_per_sentence(run=False):
         if run:
             df = pd.read_csv(
                 "data/raw_data_short_one_mask_per_sentence.csv", nrows=None)
 
-            # print(df[["masked_token",
-            #             "pos_tag",
-            #             "morph_definite",
-            #             "morph_case",
-            #             "morph_gender",
-            #             "morph_number",
-            #             "morph_pron_type",
-            #             "morph_poss"]].drop_duplicates().sort_values(["pos_tag",
-            #                                                             "morph_definite",
-            #                                                             "morph_case",
-            #                                                             "morph_gender",
-            #                                                             "morph_number",
-            #                                                             "morph_pron_type",
-            #                                                             "morph_poss"]).reset_index(drop=True))
             possible_tokens = {"die": ["der", "das", "den", "dem", "des"],
                                "der": ["die", "das", "den", "dem", "des"],
                                "das": ["die", "der", "den", "dem", "des"],
@@ -227,16 +247,10 @@ class DataGenerator:
                                "Dem": ["Die", "Der", "Das", "Den", "Des"],
                                "Des": ["Die", "Der", "Das", "Den", "Dem"], }
 
-            df = df[(df["morph_definite"] == "Def") & (df["pos_tag"] == "ART")]
+            df = df[(df["morph_definite"] == "Def") & (
+                df["pos_tag"] == "ART") & (df["morph_pron_type"] == "Art")]
             df = df[df["masked_token"].isin(possible_tokens.keys())]
 
-            lowercase_tokens = ["die", "der", "das", "den", "dem", "des"]
-            print(df[df["masked_token"].isin(lowercase_tokens)][["masked_token",
-                                                                "morph_case",
-                                                                 "morph_gender",
-                                                                 "morph_number"]].drop_duplicates().sort_values(["morph_case",
-                                                                                                                "morph_gender",
-                                                                                                                 "morph_number"]).reset_index(drop=True))
             print(df["masked_token"].unique())
             print(len(df))
 
@@ -461,6 +475,8 @@ DataGenerator.generate_training_data_one_mask_per_sentence(False)
 
 DataGenerator.generate_training_data_multiple_masks_per_sentence(False)
 # 100%|████████████████████████████████████████████████████████| 100000/100000 [21:23<00:00, 77.92it/s]
+
+DataGenerator.show_info(True)
 
 DataGenerator.add_wrong_sentences_to_training_data_one_mask_per_sentence(False)
 
